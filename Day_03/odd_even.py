0c6c9c3a41103cfd6dc252453cb097d113a9d125
Raju Das <rajudas.sit@gmail.com>
2025-09-03 14:49:06 +0530

    Updated

--- Day_03/odd_even.py
+++ Day_03/odd_even.py
@@ -5,3 +5,28 @@ if n % 2 == 0:
 else:
     print(f"{n} is an odd number")
 # int convert the input function as string to integer
+
+# Compact way write the same code
+message = "Number is even" if n % 2 == 0 else "Number is odd"
+print (message)
+
+#Using negate operator
+
+if not n % 2 == 0:
+    print(f"{n} is an odd number!")
+else:
+    print(f"{n} is an even number!")
+
+# Use AND operator
+if n > 10 and n % 2 == 0:
+    print("Yes")
+else:
+    print("No")
+
+# Use OR operator
+a = int(input("Enter an another number:"))
+if n > 10 or n % 2 == 0:
+    print("Yes")
+else:
+    print("No")
+
