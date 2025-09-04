c8b3a9b14dc03d238da2fb93ef13286063e55a13
Raju Das <rajudas.sit@gmail.com>
2025-09-04 12:25:59 +0530

    Delete Summary_of_for_loop.py

--- Day_04/Summary_of_for_loop.py
+++
@@ -1,5 +0,0 @@
-# 1. for loops iterate over a sequence, such as list, tuple, or string, excluding a block of code for each item.
-# 2. The range() functions is often used with for loops to generate a sequence of numbers, facilitating iteration over a set number of steps.
-# 3. Loop control statements like break and continue can alter the flow of a loop, with break existing the loop and continue skipping to the iteration.
-# 4. enumerate() will allow you to access both the index and the element from a list inside the for loop.
-
