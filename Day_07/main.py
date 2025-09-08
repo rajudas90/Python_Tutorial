9d1e8b900bf6914b73491c1941f9618240ed3b10
Raju Das <rajudas.sit@gmail.com>
2025-09-08 14:43:59 +0530

    Day 07 added

---
+++ Day_07/main.py
@@ -0,0 +1,9 @@
+import my_module
+
+print(my_module.greet("Raju"))
+print(my_module.add(5,3))
+
+import myutils
+
+print("Cylinder Volume is: ", myutils.find_cylinder_volume(10,7))
+print("Triangle area is: ", myutils.find_triangle_area(10, 15))
