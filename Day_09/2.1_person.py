dd0ce775ddba237d800e71a9c3f83a7485c8dfd5
Raju Das <rajudas.sit@gmail.com>
2025-09-25 17:07:11 +0530

    Day 09 Updated

--- Day_09/2.1_person.py
+++ Day_09/2.1_person.py
@@ -1,11 +1,21 @@
-class person:
+class Person:
     name= "Rohan"
     occupation = "Software Developer"
     networth = 10
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
 
-a = person()
+a = Person()
+b = Person()
+c = Person()
 a.name = "Raju"
 a.occupation = "Accountant"
-print(a.name, a.occupation) # Output is Raju Accountant
 
-# If I do not change a.name = "Raju" & a.occupation = "Accountant" then print only "Rohan Software Developer"
+b.name = "Kiran"
+b.occupation = "Data Analyst"
+# print(a.name, a.occupation) # Output is Raju Accountant
+a.info()
+b.info()
+c.info()
+# If I do not change a.name = "Raju" & a.occupation = "Accountant" then print only default value
+# As we have not mentioned any information of c, so it will take default value
